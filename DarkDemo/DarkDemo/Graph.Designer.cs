68e5ab350ff4a629d8838396221e9c5ee525bc12
M-Elsaeed <m.elsaeed1998@gmail.com>
2018-12-22 20:33:01 +0200

    a

--- DarkDemo/DarkDemo/Graph.Designer.cs
+++ DarkDemo/DarkDemo/Graph.Designer.cs
@@ -38,6 +38,8 @@
             this.button1 = new System.Windows.Forms.Button();
             this.fromText = new System.Windows.Forms.TextBox();
             this.toText = new System.Windows.Forms.TextBox();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.panel3.SuspendLayout();
             this.SuspendLayout();
             // 
             // graphBtn
@@ -47,10 +49,10 @@
             this.graphBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.graphBtn.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.graphBtn.ForeColor = System.Drawing.Color.White;
-            this.graphBtn.Location = new System.Drawing.Point(515, 25);
-            this.graphBtn.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.graphBtn.Location = new System.Drawing.Point(1093, 6);
+            this.graphBtn.Margin = new System.Windows.Forms.Padding(2);
             this.graphBtn.Name = "graphBtn";
-            this.graphBtn.Size = new System.Drawing.Size(90, 50);
+            this.graphBtn.Size = new System.Drawing.Size(128, 56);
             this.graphBtn.TabIndex = 2;
             this.graphBtn.Text = "Graph Equation";
             this.graphBtn.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
@@ -61,10 +63,10 @@
             // eqnText
             // 
             this.eqnText.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.eqnText.Location = new System.Drawing.Point(137, 32);
-            this.eqnText.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.eqnText.Location = new System.Drawing.Point(281, 19);
+            this.eqnText.Margin = new System.Windows.Forms.Padding(2);
             this.eqnText.Name = "eqnText";
-            this.eqnText.Size = new System.Drawing.Size(113, 29);
+            this.eqnText.Size = new System.Drawing.Size(183, 29);
             this.eqnText.TabIndex = 13;
             this.eqnText.Text = "In terms of X";
             // 
@@ -72,7 +74,7 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(87, 36);
+            this.label1.Location = new System.Drawing.Point(231, 23);
             this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(47, 24);
@@ -81,17 +83,17 @@
             // 
             // graphPnl
             // 
-            this.graphPnl.Location = new System.Drawing.Point(8, 88);
-            this.graphPnl.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.graphPnl.Location = new System.Drawing.Point(11, 94);
+            this.graphPnl.Margin = new System.Windows.Forms.Padding(2);
             this.graphPnl.Name = "graphPnl";
-            this.graphPnl.Size = new System.Drawing.Size(597, 307);
+            this.graphPnl.Size = new System.Drawing.Size(1228, 695);
             this.graphPnl.TabIndex = 2;
             // 
             // label2
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(267, 36);
+            this.label2.Location = new System.Drawing.Point(489, 23);
             this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(59, 24);
@@ -102,7 +104,7 @@
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(397, 36);
+            this.label3.Location = new System.Drawing.Point(715, 22);
             this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(35, 24);
@@ -116,8 +118,8 @@
             this.button1.Font = new System.Drawing.Font("Century Gothic", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.button1.ForeColor = System.Drawing.Color.White;
             this.button1.Image = ((System.Drawing.Image)(resources.GetObject("button1.Image")));
-            this.button1.Location = new System.Drawing.Point(8, 23);
-            this.button1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.button1.Location = new System.Drawing.Point(11, 11);
+            this.button1.Margin = new System.Windows.Forms.Padding(2);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(65, 51);
             this.button1.TabIndex = 2;
@@ -129,20 +131,38 @@
             // fromText
             // 
             this.fromText.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.fromText.Location = new System.Drawing.Point(327, 32);
-            this.fromText.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.fromText.Location = new System.Drawing.Point(588, 19);
+            this.fromText.Margin = new System.Windows.Forms.Padding(2);
             this.fromText.Name = "fromText";
-            this.fromText.Size = new System.Drawing.Size(68, 29);
+            this.fromText.Size = new System.Drawing.Size(99, 29);
             this.fromText.TabIndex = 17;
             // 
             // toText
             // 
             this.toText.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.toText.Location = new System.Drawing.Point(435, 34);
-            this.toText.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.toText.Location = new System.Drawing.Point(796, 20);
+            this.toText.Margin = new System.Windows.Forms.Padding(2);
             this.toText.Name = "toText";
-            this.toText.Size = new System.Drawing.Size(68, 29);
-            this.toText.TabIndex = 18;
+            this.toText.Size = new System.Drawing.Size(99, 29);
+            this.toText.TabIndex = 19;
+            this.toText.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.panel3.Controls.Add(this.button1);
+            this.panel3.Controls.Add(this.toText);
+            this.panel3.Controls.Add(this.graphBtn);
+            this.panel3.Controls.Add(this.fromText);
+            this.panel3.Controls.Add(this.eqnText);
+            this.panel3.Controls.Add(this.label1);
+            this.panel3.Controls.Add(this.label3);
+            this.panel3.Controls.Add(this.label2);
+            this.panel3.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel3.Location = new System.Drawing.Point(0, 0);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(1250, 76);
+            this.panel3.TabIndex = 20;
             // 
             // Graph
             // 
@@ -150,23 +170,17 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(41)))), ((int)(((byte)(44)))), ((int)(((byte)(51)))));
             this.ClientSize = new System.Drawing.Size(1250, 800);
-            this.Controls.Add(this.toText);
-            this.Controls.Add(this.fromText);
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.label3);
-            this.Controls.Add(this.label2);
+            this.Controls.Add(this.panel3);
             this.Controls.Add(this.graphPnl);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.eqnText);
-            this.Controls.Add(this.graphBtn);
             this.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(62)))), ((int)(((byte)(120)))), ((int)(((byte)(138)))));
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
-            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "Graph";
             this.Text = "Graph";
             this.Load += new System.EventHandler(this.Graph_Load);
+            this.panel3.ResumeLayout(false);
+            this.panel3.PerformLayout();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
@@ -180,5 +194,6 @@
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.TextBox fromText;
         private System.Windows.Forms.TextBox toText;
+        private System.Windows.Forms.Panel panel3;
     }
 }
--- DarkDemo/DarkDemo/Graph.cs
+++ DarkDemo/DarkDemo/Graph.cs
@@ -26,11 +26,34 @@ namespace DarkDemo
 
         private void graphBtn_Click(object sender, EventArgs e)
         {
-            ArrayList ys = new ArrayList(), xs = new ArrayList();
+            ArrayList xs = new ArrayList(), ys = new ArrayList();
+            float min = float.Parse(fromText.Text), max = float.Parse(toText.Text);
+            min = -500;max = 500;
+            float j = min;
+            while (j < max)
+            {
+                xs.Add(j);
+                ys.Add(-(j * j));
+                j += 0.001f;
+
+            }
+
+            Pen blackpen = new Pen(Color.Red, 3);
+            Graphics g = graphPnl.CreateGraphics();
+            for (int i = 1; i < xs.Count; i++)
+            {
+                g.DrawLine(blackpen, (float)xs[i - 1] + 500, (float)ys[i - 1]+(500*500), (float)xs[i] + 500, (float)ys[i]+ (500 * 500));
+            }
+            g.Dispose();
 
         }
 
         private void Graph_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
         {
 
         }
