c8d4d37130cc5b396c0d5ef1978e96a56da85532
M-Elsaeed <m.elsaeed1998@gmail.com>
2018-12-23 19:36:30 +0200

    created arithmetic

---
+++ DarkDemo/DarkDemo/Arithmetic.Designer.cs
@@ -0,0 +1,163 @@
+﻿namespace DarkDemo
+{
+    partial class Arithmetic
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Arithmetic));
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.closeBtn = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.eqnText = new System.Windows.Forms.TextBox();
+            this.resText = new System.Windows.Forms.TextBox();
+            this.homeBtn = new System.Windows.Forms.Button();
+            this.panel3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.panel3.Controls.Add(this.closeBtn);
+            this.panel3.Controls.Add(this.label2);
+            this.panel3.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel3.Location = new System.Drawing.Point(0, 0);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(1578, 76);
+            this.panel3.TabIndex = 2;
+            // 
+            // closeBtn
+            // 
+            this.closeBtn.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(41)))), ((int)(((byte)(44)))), ((int)(((byte)(51)))));
+            this.closeBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.closeBtn.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.closeBtn.Location = new System.Drawing.Point(1521, 12);
+            this.closeBtn.Name = "closeBtn";
+            this.closeBtn.Size = new System.Drawing.Size(45, 26);
+            this.closeBtn.TabIndex = 11;
+            this.closeBtn.Text = " X ";
+            this.closeBtn.UseVisualStyleBackColor = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Monotype Corsiva", 48F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(3, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(1135, 79);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Aritmetics, Trignometrics, Logs and Exponents";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Cursor = System.Windows.Forms.Cursors.SizeAll;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 50F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(811, 363);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(72, 76);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "=";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // eqnText
+            // 
+            this.eqnText.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(41)))), ((int)(((byte)(44)))), ((int)(((byte)(51)))));
+            this.eqnText.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.eqnText.Cursor = System.Windows.Forms.Cursors.No;
+            this.eqnText.Font = new System.Drawing.Font("Microsoft Sans Serif", 50F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.eqnText.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(62)))), ((int)(((byte)(120)))), ((int)(((byte)(138)))));
+            this.eqnText.Location = new System.Drawing.Point(18, 363);
+            this.eqnText.Name = "eqnText";
+            this.eqnText.Size = new System.Drawing.Size(787, 76);
+            this.eqnText.TabIndex = 5;
+            this.eqnText.Text = "Equation";
+            this.eqnText.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // resText
+            // 
+            this.resText.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(41)))), ((int)(((byte)(44)))), ((int)(((byte)(51)))));
+            this.resText.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.resText.Cursor = System.Windows.Forms.Cursors.No;
+            this.resText.Font = new System.Drawing.Font("Microsoft Sans Serif", 50F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.resText.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(62)))), ((int)(((byte)(120)))), ((int)(((byte)(138)))));
+            this.resText.Location = new System.Drawing.Point(889, 363);
+            this.resText.Name = "resText";
+            this.resText.Size = new System.Drawing.Size(486, 76);
+            this.resText.TabIndex = 6;
+            this.resText.Text = "Result";
+            this.resText.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // homeBtn
+            // 
+            this.homeBtn.FlatAppearance.BorderSize = 0;
+            this.homeBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.homeBtn.Font = new System.Drawing.Font("Century Gothic", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.homeBtn.ForeColor = System.Drawing.Color.White;
+            this.homeBtn.Image = ((System.Drawing.Image)(resources.GetObject("homeBtn.Image")));
+            this.homeBtn.Location = new System.Drawing.Point(12, 82);
+            this.homeBtn.Name = "homeBtn";
+            this.homeBtn.Size = new System.Drawing.Size(143, 155);
+            this.homeBtn.TabIndex = 10;
+            this.homeBtn.Text = "Home";
+            this.homeBtn.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.homeBtn.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
+            this.homeBtn.UseVisualStyleBackColor = true;
+            // 
+            // Arithmetic
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(41)))), ((int)(((byte)(44)))), ((int)(((byte)(51)))));
+            this.ClientSize = new System.Drawing.Size(1578, 1044);
+            this.Controls.Add(this.homeBtn);
+            this.Controls.Add(this.resText);
+            this.Controls.Add(this.eqnText);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.panel3);
+            this.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(62)))), ((int)(((byte)(120)))), ((int)(((byte)(138)))));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Arithmetic";
+            this.Text = "Arithmetic";
+            this.panel3.ResumeLayout(false);
+            this.panel3.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Button closeBtn;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox eqnText;
+        private System.Windows.Forms.TextBox resText;
+        private System.Windows.Forms.Button homeBtn;
+    }
+}
---
+++ DarkDemo/DarkDemo/Arithmetic.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DarkDemo
+{
+    public partial class Arithmetic : Form
+    {
+        public Arithmetic()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
